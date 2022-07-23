272a0ba3591249e0c502a4dc28daa7cc38511fe6
Jens Dietrich <jens.dietrich@gmail.com>
2022-07-24 11:43:09 +1200

    added some notes to tests

--- src/test/java/monad/MonadicLawTests.java
+++ src/test/java/monad/MonadicLawTests.java
@@ -12,19 +12,28 @@ public class MonadicLawTests {
     @Test
     public void testLeftIdentity() {
         Function<String,Provenanced<String>> prefixWithUnderscore = s -> Provenanced.of("_"+s);
-        assertEquals(Provenanced.of("foo").bind(prefixWithUnderscore),prefixWithUnderscore.apply("foo"));
+        assertEquals(
+            Provenanced.of("foo").bind(prefixWithUnderscore),  // unit x bind
+            prefixWithUnderscore.apply("foo")
+        );
     }
 
     @Test
     public void testRightIdentity() {
-        assertEquals(Provenanced.of("foo").bind(s -> Provenanced.of(s)),(Provenanced.of("foo")));
+        assertEquals(
+            Provenanced.of("foo").bind(s -> Provenanced.of(s)),  // does not double-wrap
+            (Provenanced.of("foo"))
+        );
     }
 
     @Test
     public void testAssociativity() {
         Function<String,Provenanced<String>> prefixWithUnderscore = s -> Provenanced.of("_"+s);  // f
         Function<String,Provenanced<String>> postfixWithUnderscore = s -> Provenanced.of(s+"_"); // g
-        Function<String,Provenanced<String>> padWithUnderscore = s -> Provenanced.of(s).bind(prefixWithUnderscore).bind(postfixWithUnderscore);
-        assertEquals(Provenanced.of("foo").bind(padWithUnderscore),Provenanced.of("foo").bind(prefixWithUnderscore).bind(postfixWithUnderscore));
+        Function<String,Provenanced<String>> padWithUnderscore = s -> Provenanced.of(s).bind(prefixWithUnderscore).bind(postfixWithUnderscore);  // fg
+        assertEquals(
+            Provenanced.of("foo").bind(padWithUnderscore),
+            Provenanced.of("foo").bind(prefixWithUnderscore).bind(postfixWithUnderscore)
+        );
     }
 }
